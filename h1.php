9139c1e83cd6552a77f9905a71622edcb11a6ce8
zhenggg <24505@srithatpit.ac.th>
2025-02-28 11:06:27 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+    <p>-----การสร้างตัวหนา-----</p>
+   <div>
+       <h1>HTMLเบื้องต้น(h1)</h1>
+   </div>   
+   <div>
+       <h2>HTMLเบื้องต้น(h2)</h2>
+       <h3>HTMLเบื้องต้น(h2)</h3>
+       <h4>HTMLเบื้องต้น(h2)</h4>
+       <h5>HTMLเบื้องต้น(h2)</h5>
+       <h6>HTMLเบื้องต้น(h2)</h6>
+    </div>  
+        <p>-----การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้-----</p>
+        <div>
+        <b>ฑิคัมพร สมิงไพร</b><br>
+        <i>ฑิคัมพร สมิงไพร</i><br>
+        <u>ฑิคัมพร สมิงไพร</u><br>
+    </div>
+    <p><b>-----การ Link-----</b></p>
+    <div>
+       <!--Linkหน้าเดียว-->
+       <a href = "https://www.google.co.th">Google</a><br>
+    </div>  
+</body>
+</html>
